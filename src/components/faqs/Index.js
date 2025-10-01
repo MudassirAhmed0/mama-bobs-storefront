9c91aaabaa96abf99148eb3401504d06b682a562
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2025-10-01 17:24:24 +0500

    origin

--- src/components/faqs/Index.js
+++ src/components/faqs/Index.js
@@ -6,45 +6,56 @@ import LogoBackground from "../common/LogoBackground";
 const FaqsPage = () => {
   const faqs = [
     {
-      question: "Do we offer international shipping?",
-      answer: "Yes, on orders of 3 items or more",
+      question: "What are dangleberries?",
+      answer: "DANGLEBERRIE - bead of self produced ecsrement that has attached itself to your hairy undercarriage! And dingle dangles in that region hence DANGLEBERRIE!!",
     },
     {
-      question: "Can I track my order?",
-      answer:
-        "Yes, you will receive a tracking link, once your order is shipped",
+      question: "Return Policy?",
+      answer: "God knows why you want to return a piece of MAMA BOBS quality apparel. But if you do, any item can be returned within 30 days of purchase.  (This should be at the bottom of the list of what the FAQ)",
+    },
+    {
+      question: "Do We Offer International Shipping?",
+      answer: "YES we offer International shipping on the order of 5 Items or more.",
+    },
+    {
+      question: "What is feltching and explanation?",
+      answer: "A sexual act term commonly used in the LGBTQIA+ community. It is in layman's terms when one homosexual male ejaculates in another homosexual males nether region orifice and then make the highly dubious decision to go and suck his deposit back out again!",
+    },
+    {
+      question: "How can I track my order?",
+      answer: "Once your order is shipped you will receive a tracking link via email.",
     },
     {
       question: "What payment methods do you accept?",
-      answer: "List of all payment: Credit / Debit Card Master & Visa",
+      answer: "We accept online payment made through any of your Debit/Credit cards.",
     },
     {
       question: "Can I contact customer support?",
-      answer:
-        "Absolutely. Tel: +61467395529 Email: admin@mamabobs.com",
+      answer: "YES you can reach out to us Via email, Whatsapp and can also give us a call on this number +61467-395-529",
     },
-    
+    {
+      question: "Do we offer a discount on bulk orders?",
+      answer: "Not Right now but we will come up with something very soon.",
+    },
+    {
+      question: "What is the meaning of life?",
+      answer: "The aim is to secure yourself a life of peace, contentment and love by living with positivity and above all in any situation- BE KIND! BE KIND! BE KIND!",
+    },
     {
       question: "Can I change or cancel my order?",
-      answer: "Absolutely. You Can just contact us. Tel: +61467395529 Email: admin@mamabobs.com",
+      answer: "Sure you can by reaching out to the customer support via any channel.",
     },
     {
       question: "How long does delivery take?",
-      answer:
-        "will find out for sure when we know which courier we are going to use.",
+      answer: "Standard Delivery may take upto 5-7 business days and Express Delivery will take around 2-4 Business days.",
     },
     {
-      question: " Do you have a loyalty program?",
-      answer:
-        " If you show your loyalty to the MAMA BOBS movement by sending us a photo of you wearing MAMA BOBS any photo that we use , you will receive 20% off on your next order. The stranger the place the larger the discount! ( THE GAUNTLET HAS BEEN THROWN DOWN!!)",
+      question: "Do you have a loyalty program?",
+      answer: "If you show your loyalty to the MAMA BOBS movement by sending us a photo of you wearing MAMA BOBS any photo that we use , you will receive 20% off on your next order. The stranger the place the larger the discount! ( THE GAUNTLET HAS BEEN THROWN DOWN!!)",
     },
-
-    {
-      question: "What is our return policy?",
-      answer:
-        "God knows why you want to return a piece of MAMA BOBS quality apparel. But if you do, any item can be returned within 30 days of purchase.",
-    },
+   
   ];
+  
   const [openIndex, setOpenIndex] = useState(0);
   const contentRefs = useRef([]);
   const [heights, setHeights] = useState({});
